5db8dcbff1cbb431d1491852c37c65853f15d001
Süleyman <Suleymanpsp38@gmail.com>
2020-12-16 23:20:13 +0300

    Edit1

---
+++ Bc.Vc-Clicked_Bot/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Service
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox_Linklist = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbxDriverHideAndShow = new System.Windows.Forms.CheckBox();
+            this.cbxBaslangicdaCalistir = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.cbxFormGizle = new System.Windows.Forms.CheckBox();
+            this.cbxMesajVer = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox_Linklist
+            // 
+            this.listBox_Linklist.BackColor = System.Drawing.SystemColors.Control;
+            this.listBox_Linklist.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox_Linklist.ForeColor = System.Drawing.Color.Olive;
+            this.listBox_Linklist.FormattingEnabled = true;
+            this.listBox_Linklist.Location = new System.Drawing.Point(9, 11);
+            this.listBox_Linklist.Name = "listBox_Linklist";
+            this.listBox_Linklist.Size = new System.Drawing.Size(194, 277);
+            this.listBox_Linklist.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(44, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "....";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(55, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 18);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(102, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 18);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Navy;
+            this.label4.Location = new System.Drawing.Point(79, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 18);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "/";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.ForeColor = System.Drawing.Color.Navy;
+            this.groupBox1.Location = new System.Drawing.Point(207, 168);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(194, 55);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Total";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(209, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 42);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Linkleri Güncelle";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(10, 294);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(192, 44);
+            this.button2.TabIndex = 21;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold);
+            this.groupBox2.ForeColor = System.Drawing.Color.Navy;
+            this.groupBox2.Location = new System.Drawing.Point(207, 229);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(194, 52);
+            this.groupBox2.TabIndex = 23;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Clicked now";
+            // 
+            // cbxDriverHideAndShow
+            // 
+            this.cbxDriverHideAndShow.AutoSize = true;
+            this.cbxDriverHideAndShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cbxDriverHideAndShow.ForeColor = System.Drawing.Color.Navy;
+            this.cbxDriverHideAndShow.Location = new System.Drawing.Point(7, 26);
+            this.cbxDriverHideAndShow.Name = "cbxDriverHideAndShow";
+            this.cbxDriverHideAndShow.Size = new System.Drawing.Size(131, 22);
+            this.cbxDriverHideAndShow.TabIndex = 24;
+            this.cbxDriverHideAndShow.Text = "Chrome Gizle";
+            this.cbxDriverHideAndShow.UseVisualStyleBackColor = true;
+            // 
+            // cbxBaslangicdaCalistir
+            // 
+            this.cbxBaslangicdaCalistir.AutoSize = true;
+            this.cbxBaslangicdaCalistir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cbxBaslangicdaCalistir.ForeColor = System.Drawing.Color.Navy;
+            this.cbxBaslangicdaCalistir.Location = new System.Drawing.Point(7, 56);
+            this.cbxBaslangicdaCalistir.Name = "cbxBaslangicdaCalistir";
+            this.cbxBaslangicdaCalistir.Size = new System.Drawing.Size(172, 22);
+            this.cbxBaslangicdaCalistir.TabIndex = 25;
+            this.cbxBaslangicdaCalistir.Text = "Başlangıçta Çalıştır";
+            this.cbxBaslangicdaCalistir.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cbxMesajVer);
+            this.groupBox3.Controls.Add(this.cbxFormGizle);
+            this.groupBox3.Controls.Add(this.cbxDriverHideAndShow);
+            this.groupBox3.Controls.Add(this.cbxBaslangicdaCalistir);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox3.ForeColor = System.Drawing.Color.Maroon;
+            this.groupBox3.Location = new System.Drawing.Point(209, 11);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(192, 151);
+            this.groupBox3.TabIndex = 26;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Settings";
+            // 
+            // cbxFormGizle
+            // 
+            this.cbxFormGizle.AutoSize = true;
+            this.cbxFormGizle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cbxFormGizle.ForeColor = System.Drawing.Color.Navy;
+            this.cbxFormGizle.Location = new System.Drawing.Point(7, 86);
+            this.cbxFormGizle.Name = "cbxFormGizle";
+            this.cbxFormGizle.Size = new System.Drawing.Size(120, 22);
+            this.cbxFormGizle.TabIndex = 26;
+            this.cbxFormGizle.Text = "Formu Gizle";
+            this.cbxFormGizle.UseVisualStyleBackColor = true;
+            // 
+            // cbxMesajVer
+            // 
+            this.cbxMesajVer.AutoSize = true;
+            this.cbxMesajVer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cbxMesajVer.ForeColor = System.Drawing.Color.Navy;
+            this.cbxMesajVer.Location = new System.Drawing.Point(7, 116);
+            this.cbxMesajVer.Name = "cbxMesajVer";
+            this.cbxMesajVer.Size = new System.Drawing.Size(158, 22);
+            this.cbxMesajVer.TabIndex = 27;
+            this.cbxMesajVer.Text = "Bitince Mesaj Ver";
+            this.cbxMesajVer.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 343);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox_Linklist);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Service.exe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox_Linklist;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox cbxDriverHideAndShow;
+        private System.Windows.Forms.CheckBox cbxBaslangicdaCalistir;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox cbxFormGizle;
+        private System.Windows.Forms.CheckBox cbxMesajVer;
+    }
+}
+
---
+++ Bc.Vc-Clicked_Bot/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System.Net;
+using HtmlAgilityPack;
+using System.IO;
+using Microsoft.Win32;
+using Microsoft.Win32.TaskScheduler;
+
+namespace Service
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+
+        }
+
+        IWebDriver driver;
+        public async void DriverBaslat()
+        {
+
+            ChromeOptions options = new ChromeOptions();
+
+            options.AddArguments("--disable-notifications");
+
+            if (cbxDriverHideAndShow.Checked == true)
+            {
+                options.AddArgument("--headless");
+            }
+
+            options.AddExcludedArgument("enable-automation");
+
+            ChromeDriverService cds = ChromeDriverService.CreateDefaultService();
+            cds.HideCommandPromptWindow = true;
+
+            driver = new ChromeDriver(cds, options);
+
+            await System.Threading.Tasks.Task.Delay(2000);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LinkleriCek();
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            LinkleriCek();
+        }
+
+        private void LinkleriCek()
+        {
+            StreamReader oku = File.OpenText(AppDomain.CurrentDomain.BaseDirectory + "\\BCVC_Links.txt");
+            string yazi;
+            while ((yazi = oku.ReadLine()) != null) //satır boş olana kadar satır satır okumaya devam eder
+            {
+                listBox_Linklist.Items.Add(yazi.ToString());
+            }
+            oku.Close();//okumayı kapat
+            label3.Text = listBox_Linklist.Items.Count.ToString();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (cbxBaslangicdaCalistir.Checked == true)
+            {
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+                key.SetValue("Service.exe", "\"" + Application.ExecutablePath + "\"");
+            }
+
+            await System.Threading.Tasks.Task.Run(async () =>
+            {
+                if (cbxFormGizle.Checked == true)
+                {
+                    this.Hide();
+                }
+
+
+                try
+                {
+                    for (int i = 0; i < listBox_Linklist.Items.Count; i++)
+                    {
+
+                        listBox_Linklist.SelectedIndex = i;
+                        label1.Text = listBox_Linklist.SelectedItem.ToString();
+
+                        label3.Text = listBox_Linklist.Items.Count.ToString();
+                        label2.Text = listBox_Linklist.SelectedIndex.ToString();
+                        DriverBaslat();
+
+                        driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+                        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+                        driver.Navigate().GoToUrl(listBox_Linklist.SelectedItem.ToString());
+                        await System.Threading.Tasks.Task.Delay(10000);//10 saniye beklet
+                        IWebElement ReklamiGec = driver.FindElement(By.CssSelector("#getLink"));
+                        ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", ReklamiGec);
+                        driver.SwitchTo().Window(driver.WindowHandles.First());
+                        await System.Threading.Tasks.Task.Delay(5000);
+                        driver.SwitchTo().Window(driver.WindowHandles.First());
+
+                        driver.Close();
+                        driver.Quit();
+                        driver.Dispose();
+
+                    }
+                    if (cbxMesajVer.Checked == true)
+                    {
+                        MessageBox.Show("Finish!");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    this.Close();
+                    Application.Exit();
+                    driver.Close();
+                    driver.Quit();
+                    driver.Dispose();
+                }
+
+            });
+        }
+    }
+}
